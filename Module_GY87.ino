3e1c55b2539a613e3b9875fb149253b0406e74c5
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-05-01 03:27:53 -0300

    [@Lohkdesgds] Fully updated to newest format and enhanced readings using examples of mpu device

--- Module_GY87.ino
+++ Module_GY87.ino
@@ -1,5 +1,6 @@
 #include "mcombo.h"
 #include "Serial/packaging.h"
+#include "Serial/flags.h"
 
 using namespace CS;
 
@@ -11,9 +12,6 @@ void callback(void*, const uint8_t, const char*, const uint8_t);
 
 void setup() {
     Serial.begin(115200);
-    while(!Serial);
-
-    Serial.printf("Starting SLAVE\n");
     
     mcb = new mGY87();
     
@@ -34,88 +32,140 @@ void callback(void* rw, const uint8_t expects, const char* received, const uint8
     switch(req.get_offset()) {
     case 0:
     {
+        FlagWrapper fw;
+        if (mcb->has_issues())              fw |= device_flags::HAS_ISSUES;
+        if (mcb->has_new_data_autoreset())  fw |= device_flags::HAS_NEW_DATA;
+        
+        Command cmd("#FLAGS", (uint64_t)fw);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 1:
+    {
         const float val = mcb->get_temperature();
         Command cmd("/gy87/bmp085/temperature", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 1:
+    case 2:
     {
         const int64_t val = mcb->get_pressure_pa();
         Command cmd("/gy87/bmp085/pressure", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 2:
+    case 3:
     {
         const float val = mcb->get_altitude();
         Command cmd("/gy87/bmp085/altitude", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 3:
+    case 4:
     {
         const int64_t val = mcb->get_pressure_seal_level_pa();
         Command cmd("/gy87/bmp085/pressure_sea_level", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 4:
+    case 5:
     {
         const float val = mcb->get_real_altitude();
         Command cmd("/gy87/bmp085/altitude_real", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 5:
+    case 6:
     {
-        const float val = mcb->get_heading();
-        Command cmd("/gy87/hmc58831/heading", val);
+        const int64_t val = mcb->get_accel_x_raw();
+        Command cmd("/gy87/mpu6050/accel/x/raw", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 6:
+    case 7:
+    {
+        const int64_t val = mcb->get_accel_y_raw();
+        Command cmd("/gy87/mpu6050/accel/y/raw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 8:
+    {
+        const int64_t val = mcb->get_accel_z_raw();
+        Command cmd("/gy87/mpu6050/accel/z/raw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 9:
+    {
+        const int64_t val = mcb->get_gyro_x_raw();
+        Command cmd("/gy87/mpu6050/gyro/x/raw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 10:
+    {
+        const int64_t val = mcb->get_gyro_y_raw();
+        Command cmd("/gy87/mpu6050/gyro/y/raw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 11:
+    {
+        const int64_t val = mcb->get_gyro_z_raw();
+        Command cmd("/gy87/mpu6050/gyro/z/raw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 12:
+    {
+        const int64_t val = mcb->get_yaw();
+        Command cmd("/gy87/mpu6050/yaw", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 13:
+    {
+        const int64_t val = mcb->get_pitch();
+        Command cmd("/gy87/mpu6050/pitch", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 14:
+    {
+        const int64_t val = mcb->get_roll();
+        Command cmd("/gy87/mpu6050/roll", val);
+        w.slave_reply_from_callback(cmd);
+    }
+    break;
+    case 15:
     {
         const int64_t val = mcb->get_accel_x();
         Command cmd("/gy87/mpu6050/accel/x", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 7:
+    case 16:
     {
         const int64_t val = mcb->get_accel_y();
         Command cmd("/gy87/mpu6050/accel/y", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 8:
+    case 17:
     {
         const int64_t val = mcb->get_accel_z();
         Command cmd("/gy87/mpu6050/accel/z", val);
         w.slave_reply_from_callback(cmd);
     }
     break;
-    case 9:
+    /*case 18:
     {
-        const int64_t val = mcb->get_gyro_x();
-        Command cmd("/gy87/mpu6050/gyro/x", val);
+        const float val = mcb->get_heading();
+        Command cmd("/gy87/hmc58831/heading", val);
         w.slave_reply_from_callback(cmd);
     }
-    break;
-    case 10:
-    {
-        const int64_t val = mcb->get_gyro_y();
-        Command cmd("/gy87/mpu6050/gyro/y", val);
-        w.slave_reply_from_callback(cmd);
-    }
-    break;
-    case 11:
-    {
-        const int64_t val = mcb->get_gyro_z();
-        Command cmd("/gy87/mpu6050/gyro/z", val);
-        w.slave_reply_from_callback(cmd);
-    }
-    break;
+    break;*/
     default:
     {
         Command cmd; // invalid
--- mcombo.h
+++ mcombo.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "I2Cdev/Arduino/MPU6050/MPU6050.h"
+#include "I2Cdev/Arduino/MPU6050/MPU6050_6Axis_MotionApps20.h"
 #include "Adafruit-BMP085-Library/Adafruit_BMP085.h"
 #include "HMC5883L_Simple/HMC5883L_Simple/HMC5883L_Simple.h"
 
@@ -15,20 +15,25 @@ class mGY87 : protected SelfThreadable {
     int32_t m_sea_level_pressure = 0; // Pa
     float m_real_altitude = 0.0f; // meters
     // Compass hmc5883l
-    float m_heading = 0.0f;
+    //float m_heading = 0.0f;
     // MPU6050
     int16_t m_ax = 0, m_ay = 0, m_az = 0;
     int16_t m_gx = 0, m_gy = 0, m_gz = 0;
+    float m_yaw_pitch_roll[3]{0.0f};
+    float m_acceleration[3]{0.0f};
+    VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
     
-    bool m_fail_flag = true;
     
+    bool m_fail_flag = false;
+    bool m_has_new_data = false;    
 
     void async() {
-        MPU6050 accelgyro;
+        MPU6050 mpu;
         Adafruit_BMP085 bmp;
-        HMC5883L_Simple Compass;
+        //HMC5883L_Simple Compass;
         
         Wire.begin();
+        Wire.setClock(400000);
                 
         while(!bmp.begin()){
             m_fail_flag = true;
@@ -36,53 +41,94 @@ class mGY87 : protected SelfThreadable {
         }
         
         // initialize mpu6050
-        accelgyro.initialize();
-        while(!accelgyro.testConnection()) {
+        mpu.initialize();
+        while(!mpu.testConnection()) {
             m_fail_flag = true;
             delay(200);
         }
-        accelgyro.setI2CBypassEnabled(true); // set bypass mode for gateway to hmc5883L
+        //mpu.setI2CBypassEnabled(true); // set bypass mode for gateway to hmc5883L
+        if (mpu.dmpInitialize() == 0) mpu.setDMPEnabled(true);
 
         // initialize hmc5883l
-        Compass.SetDeclination(23, 35, 'E');
-        Compass.SetSamplingMode(COMPASS_SINGLE);
-        Compass.SetScale(COMPASS_SCALE_130);
-        Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+        //Compass.SetDeclination(23, 35, 'E');
+        //Compass.SetSamplingMode(COMPASS_CONTINUOUS);
+        //Compass.SetScale(COMPASS_SCALE_130);
+        //Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
         
         m_fail_flag = false;
-        while(1) {
-            delay(1000);
-            
+        
+        Quaternion q;           // [w, x, y, z]         quaternion container
+        VectorFloat gravity;    // [x, y, z]            gravity vector
+        VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+
+        uint8_t fifoBuffer[64]; // FIFO storage buffer
+        uint16_t fifoCount;
+        const uint16_t packetSize = mpu.dmpGetFIFOPacketSize();
+        
+        while(1) {            
             m_temp = bmp.readTemperature();
             m_pressure = bmp.readPressure();
             m_altitude = bmp.readAltitude();
             m_sea_level_pressure = bmp.readSealevelPressure();
             m_real_altitude = bmp.readAltitude(101500);
             
-            accelgyro.getMotion6(&m_ax, &m_ay, &m_az, &m_gx, &m_gy, &m_gz);
+            mpu.getMotion6(&m_ax, &m_ay, &m_az, &m_gx, &m_gy, &m_gz);
             
-            m_heading = Compass.GetHeadingDegrees();
+            //m_heading = Compass.GetHeadingDegrees();
+                        
+            auto mpuIntStatus = mpu.getIntStatus();
+            fifoCount = mpu.getFIFOCount();
+            // check for overflow (this should never happen unless our code is too inefficient)
+            if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+                // reset so we can continue cleanly
+                mpu.resetFIFO();
+            }
+            else if (mpuIntStatus & 0x02) {
+                while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+                
+                mpu.getFIFOBytes(fifoBuffer, packetSize);
+                fifoCount -= packetSize;
+                
+                mpu.dmpGetQuaternion(&q, fifoBuffer);
+                mpu.dmpGetGravity(&gravity, &q);
+                mpu.dmpGetYawPitchRoll(m_yaw_pitch_roll, &q, &gravity);
+                mpu.dmpGetAccel(&aa, fifoBuffer);
+                mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+                
+                for(auto& i : m_yaw_pitch_roll) i *= 180.0f / M_PI;
+            }
+            
+            
+            m_has_new_data = true;
         }
 
         vTaskDelete(NULL);
     }
 public:
-    mGY87() : SelfThreadable("ASYNC_CCS") { async_start(); }
+    mGY87() : SelfThreadable("ASYNC") { async_start(); }
 
     float get_temperature() const { return m_temp; }
     int32_t get_pressure_pa() const { return m_pressure; }
     float get_altitude() const { return m_altitude; }
     int32_t get_pressure_seal_level_pa() const { return m_sea_level_pressure; }
     float get_real_altitude() const { return m_real_altitude; }
-    float get_heading() const { return m_heading; }
+    //float get_heading() const { return m_heading; }
     
-    int16_t get_accel_x() const { return m_ax; }
-    int16_t get_accel_y() const { return m_ay; }
-    int16_t get_accel_z() const { return m_az; }
-    int16_t get_gyro_x() const { return m_gx; }
-    int16_t get_gyro_y() const { return m_gy; }
-    int16_t get_gyro_z() const { return m_gz; }
+    int16_t get_accel_x_raw() const { return m_ax; }
+    int16_t get_accel_y_raw() const { return m_ay; }
+    int16_t get_accel_z_raw() const { return m_az; }
+    int16_t get_gyro_x_raw() const { return m_gx; }
+    int16_t get_gyro_y_raw() const { return m_gy; }
+    int16_t get_gyro_z_raw() const { return m_gz; }
     
-    bool has_issues() const;
+    float get_yaw() const { return m_yaw_pitch_roll[0]; }
+    float get_pitch() const { return m_yaw_pitch_roll[1]; }
+    float get_roll() const { return m_yaw_pitch_roll[2]; }
+    
+    int16_t get_accel_x() const { return aaReal.x; }
+    int16_t get_accel_y() const { return aaReal.y; }
+    int16_t get_accel_z() const { return aaReal.z; }
+    
+    bool has_issues() const { return m_fail_flag; }
+    bool has_new_data_autoreset() { bool had = m_has_new_data; m_has_new_data = false; return had; }
 };
-
